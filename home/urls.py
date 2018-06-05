f9959444fab37d00a764713813fe1a9e37bc9359
Arvind Tiwari <tiwariav@gmail.com>
2018-06-05 11:31:45 +0530

    gallery link

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url , include
 
 from . import views
 
+
+app_name = 'home'
+
 urlpatterns = [
     url(r'^gallery', views.gallery_view, name='gallery'),
     url(r'^', views.index, name='index'),
